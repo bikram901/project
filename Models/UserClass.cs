53f408499fe68db1c4f17d36b99e6e0a5ce753f8
bikram901 <145909657+bikram901@users.noreply.github.com>
2023-09-24 13:39:32 +0100

    Add files via upload

---
+++ Models/UserClass.cs
@@ -0,0 +1,28 @@
+﻿namespace Crud_Operation.Models
+{
+	public class UserClass
+	{
+		public string name { get; set; }
+		public String mob { get; set; }
+		public string email { get; set; }
+		public string dob { get; set; }
+		public string username { get; set; }
+		public string password { get; set; }
+
+		public string userrole { get; set; }
+
+	}
+
+	public class UserLogin
+	{
+        public string name { get; set; }
+        public String mob { get; set; }
+        public string email { get; set; }
+        public string dob { get; set; }
+        public string username { get; set; }
+		public string password { get; set; }
+		public string userrole { get; set; }
+		public int Status {  get; set; }
+
+	}
+}
